c74cc140c0e170608f08ddc8b919a86739b2370b
ArkhamCookie <github@mail.theuntitledproject.dev>
2023-12-26 18:06:14 -0600

    Create first test (Card)

---
+++ tests/card_test.js
@@ -0,0 +1,20 @@
+import { assert, assertEquals } from 'assert'
+import { Card } from '../public/js/lib/mod.js'
+
+Deno.test('Card: create a 10 of clubs', () => {
+	const card = new Card('clubs', 10)
+	assertEquals(card.suit, 'clubs')
+	assertEquals(card.rank, 10)
+})
+
+Deno.test('Card: random()', () => {
+	const card = new Card()
+	card.random()
+	assert(card.suit)
+	assert(card.rank)
+})
+
+Deno.test('Card: toString()', () => {
+	const card = new Card('clubs', 10)
+	assertEquals(card.toString(), '10 of clubs')
+})
