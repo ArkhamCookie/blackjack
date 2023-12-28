3c6e3c97e5bf0481b91c2302ed12d59a234bda5d
ArkhamCookie <github@mail.theuntitledproject.dev>
2023-12-28 14:25:03 -0600

    Start on Deck class

---
+++ public/js/lib/deck.js
@@ -0,0 +1,39 @@
+export class Deck {
+	constructor(deckAmount) {
+		this.suits = ['clubs', 'diamonds', 'hearts', 'spades']
+		this.ranks = [
+			'2', '3', '4', '5', '6', '7', '8', '9', '10',
+			'J', 'Q', 'K', 'A'
+		]
+		this.deck = []
+		if (typeof (deckAmount) === 'number') {
+			this.deckAmount = deckAmount
+		} else {
+			this.deckAmount = 1
+		}
+		this.cardsRemaining = 52 * deckAmount
+		this.dealtCards = 0
+	}
+
+	shuffle() {
+		for (let currentIndex = this.deck.length - 1; currentIndex > 0; currentIndex--) {
+			const randomIndex = Math.floor(Math.random() * (currentIndex + 1))
+			const tempCard = this.deck[currentIndex]
+
+			this.deck[currentIndex] = this.deck[randomIndex]
+			this.deck[randomIndex] = tempCard
+		}
+		this.dealtCards = 0
+		this.cardsRemaining = 52
+	}
+
+	deal() {
+		if (this.dealtCards === this.deck.length) {
+			Error('No more cards to deal')
+		}
+		this.dealtCards++
+		this.cardsRemaining--
+
+		return this.deck[this.dealtCards - 1]
+	}
+}
